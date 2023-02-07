b95ac25571974499f92af55cb9edd27272928157
Nathan Woodburn <github@nathan.woodburn.au>
2023-02-07 18:24:05 +1100

    Updated replication script to work with existing varo setup

--- sql/mbackup.sh
+++ sql/mbackup.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
-oldbind="bind-address            = 127.0.0.1"
-newbind="bind-address            = 0.0.0.0"
-
-# Check if file already changed
-if grep -q "$newbind" "/etc/mysql/mariadb.conf.d/50-server.cnf"; then
-    echo "File already changed"
-    exit 0
-fi
-
-apt-get install mariadb-server mariadb-client -y
-
-# Replace old bind address with new bind address
-sed -i "s/$oldbind/$newbind/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# Add replication settings
-echo "server-id = 1" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_bin_index =/var/log/mysql/mysql-bin.log.index" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "relay_log = /var/log/mysql/mysql-relay-bin" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "relay_log_index = /var/log/mysql/mysql-relay-bin.index" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# Restart MySQL
-systemctl restart mariadb
 
 # Generate random password
 password=$(openssl rand -base64 32)
