1a122c2d4bf3c83c77f62feba91b4be3211c3188
Nathan Woodburn <github@nathan.woodburn.au>
2023-04-30 20:09:36 +1000

    hsdDiscord: Split connections types over separate lines

--- hsdDiscord.sh
+++ hsdDiscord.sh
@@ -7,5 +7,6 @@ TYPES=$(hsd-cli rpc getpeerinfo --url=127.0.0.1 --api-key=$KEY | jq '. | map(.su
 URL="$(hsd-cli rpc getpeerinfo --url=127.0.0.1 --api-key=$KEY | jq -r '.[]|select(.inbound==true)|.addr|sub(":.*$";"")' | curl -s -XPOST --data-binary @- "ipinfo.io/tools/summarize-ips?cli=1"|jq -r .reportUrl)"
 CLEANED=$(echo $TYPES | sed 's/.*{\(.*\)}.*$/\1/')
 CLEANED=$(echo $CLEANED | sed 's/"/ /g')
+CLEANED=$(echo $CLEANED | sed 's/,/\\n /g')
 JSON="{\"content\": \"Current Height: $BLOCK\nUsers connected to node: $INBOUND\",\"embeds\": [{\"title\": \"Current Connections\",\"description\": \"$CLEANED\",\"url\": \"$URL\",\"color\": null,\"footer\": {\"text\": \"PS click the title to get a map of connections\"}}],\"attachments\": []}"
 curl -H "Content-Type: application/json" -X POST -d "$JSON" $DISCORD
